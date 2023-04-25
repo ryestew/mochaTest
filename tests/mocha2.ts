a7d2cb39925c0119d83523941b70442d7d0cd2c9
ryestew <rob@stupay.com>
2023-04-25 09:58:38 -0400

    update yml again

--- tests/mocha2.ts
+++ tests/mocha2.ts
@@ -1,10 +1,11 @@
 import { ethers } from 'ethers'
 const { expect } = require("chai")
+const fs = require('fs')
 
 describe("Storage", function () {
   it("test initial value", async function () {
     // Make sure contract is compiled and artifacts are generated
-    const metadata = JSON.parse(await remix.call('fileManager', 'getFile', 'contracts/artifacts/MyResolver.json'))
+    const metadata = JSON.parse(fs.readFileSync( 'contracts/artifacts/MyResolver.json'))
     const signer = (new ethers.providers.Web3Provider(web3Provider)).getSigner()
     let MyResolver = new ethers.ContractFactory(metadata.abi, metadata.data.bytecode.object, signer);
     let myResolver = await MyResolver.deploy();
