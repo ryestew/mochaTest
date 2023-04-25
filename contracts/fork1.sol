c304dfd7f4386ab597b4205a71cc08e0c7ebe2be
ryestew <rob@stupay.com>
2023-04-24 22:23:50 -0400

    update sol version

--- contracts/fork1.sol
+++ contracts/fork1.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.8.2 <0.9.0;
-
+pragma solidity >=0.8.10 <0.9.0;
 
 import "https://github.com/ensdomains/ens-contracts/blob/master/contracts/utils/NameEncoder.sol";
         abstract contract ENS {
