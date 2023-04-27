13f56465590dd6ec930d01824cf4c837a6c034dc
ryestew <rob@stupay.com>
2023-04-27 08:29:07 -0400

    relative path

--- tests/mocha2.ts
+++ tests/mocha2.ts
@@ -1,15 +1,15 @@
 import { ethers } from 'ethers'
 const { expect } = require("chai")
-const fs = require('fs')
+const fs = require("fs")
 
 describe("Storage", function () {
   it("test initial value", async function () {
     // Make sure contract is compiled and artifacts are generated
-    const metadata = JSON.parse(fs.readFileSync( 'contracts/artifacts/MyResolver.json'))
+    const metadata = JSON.parse(fs.readFileSync('../contracts/artifacts/MyResolver.json'))
     const signer = (new ethers.providers.Web3Provider(web3Provider)).getSigner()
     let MyResolver = new ethers.ContractFactory(metadata.abi, metadata.data.bytecode.object, signer);
     let myResolver = await MyResolver.deploy();
-    console.log('myResolver contract Address: ' + myResolver.address)
+    console.log('myResolver conytract Address: ' + myResolver.address)
     await myResolver.deployed()
     let myAddr = await myResolver.resolve()
     console.log('myResolver resolve: ' + myAddr)
