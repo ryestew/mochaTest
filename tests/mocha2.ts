ce980815c45a8d7474d15d36cf90fc83284cb9c1
ryestew <rob@stupay.com>
2023-04-27 08:40:20 -0400

    cwd it

--- tests/mocha2.ts
+++ tests/mocha2.ts
@@ -5,8 +5,8 @@ const fs = require("fs")
 describe("Storage", function () {
   it("test initial value", async function () {
     // Make sure contract is compiled and artifacts are generated
-    const metadata = JSON.parse(fs.readFileSync('../contracts/artifacts/MyResolver.json'))
     console.log('process.cwd(): ', process.cwd())
+    const metadata = JSON.parse(fs.readFileSync('../contracts/artifacts/MyResolver.json'))
     const signer = (new ethers.providers.Web3Provider(web3Provider)).getSigner()
     let MyResolver = new ethers.ContractFactory(metadata.abi, metadata.data.bytecode.object, signer);
     let myResolver = await MyResolver.deploy();
