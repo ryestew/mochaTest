c982f018a2fcc8d5ba0314363f23c3eb7edd6501
ryestew <rob@stupay.com>
2023-05-23 23:32:17 -0400

    update mocha2

--- tests/mocha2.ts
+++ tests/mocha2.ts
@@ -1,18 +1,18 @@
 import { ethers } from 'ethers'
 const { expect } = require("chai")
-const fs = require("fs")
 
 describe("Storage", function () {
   it("test initial value", async function () {
-    // Make sure contract is compiled and artifacts are generated
-    // console.log('process.cwd(): ', process.cwd())
-    const metadata = JSON.parse(fs.readFileSync('./contracts/artifacts/MyResolver.json'))
-    // console.log('metadata: ', metadata)
-    const signer = (new ethers.providers.Web3Provider(web3Provider)).getSigner()
-    let MyResolver = new ethers.ContractFactory(metadata.abi, metadata.data.bytecode.object, signer);
-    let myResolver = await MyResolver.deploy();
-    console.log('myResolver conytract Address: ' + myResolver.address)
+    const signer = await ethers.getSigners();
+    // let MyResolver = new ethers.ContractFactory(metadata.abi, metadata.data.bytecode.object, signer[0]);
+    const MyResolver = await ethers.getContractFactory("MyResolver");
+
+   let myResolver = await MyResolver.connect(signer[0]).deploy();
+    // const result = await deploy('Storage', [])
+    // let myResolver = await MyResolver.deploy();
+
     await myResolver.deployed()
+    console.log('myResolver conytract Address: ' + myResolver.address)
     let myAddr = await myResolver.resolve()
     console.log('myResolver resolve: ' + myAddr)
     expect(myAddr).to.equal('0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045') 
